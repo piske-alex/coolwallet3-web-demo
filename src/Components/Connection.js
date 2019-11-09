2d46bcae4208d88f1911c091681f42037d82f0fc
kunming.liu <kunming@coolbitx.com>
2019-11-09 20:53:21 +0800

    use WebBleTransport to scan and connect bluetooth

--- src/Components/Connection.js
+++ src/Components/Connection.js
@@ -3,20 +3,17 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
 
-
 class Ble extends Component {
-
-
   render() {
     return (
       <Container>
-          <h4> Connection </h4>
-		  <Row>
-		  	<Button variant="light" style={{margin:20}} onClick={this.props.transport.connect}>Connect</Button>
-			<Button variant="outline-warning" style={{margin:20}} onClick={this.props.transport.disconnect}> Disconnect</Button>
-		  </Row>
-	  </Container>	  
-        
+        <h4> Connection </h4>
+        <Row>
+          <Button variant="light" style={{ margin: 20 }} onClick={this.props.connect}>Connect</Button>
+          <Button variant="outline-warning" style={{ margin: 20 }} onClick={this.props.disconnect}> Disconnect</Button>
+        </Row>
+      </Container>
+
     )
   }
 }
