c3adfc10ce36c6ce705477de1df240b2a1f109ee
Anton Cheng <anton@coolbitx.com>
2020-03-17 18:42:04 +0800

    Disable button when signing transactions

--- src/Components/Ethereum/index.js
+++ src/Components/Ethereum/index.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import FormText from 'react-bootstrap/FormText';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
+import {
+  Row,
+  Col,
+  FormText,
+  InputGroup,
+  FormControl,
+  Container,
+  Form,
+  Button,
+} from 'react-bootstrap';
 import Web3 from 'web3';
 import GasMenu from './GasMenu';
 import cwsETH from '@coolwallets/eth';
@@ -18,25 +20,39 @@ function EthTest({ transport, appPrivateKey, appId }) {
   const ETH = new cwsETH(transport, appPrivateKey, appId);
 
   const [addressIndex, setAddressIndex] = useState(0);
-  // const [gasLimit, setGasLimit] = useState(21000)
   const [gasPrice, setGasPrice] = useState(10);
-  // const [nonce, setNonce] = useState(0)
   const [balance, setBalance] = useState(0);
 
+  // sign transaction
   const [address, setAddress] = useState('');
-  const [to, setTo] = useState('0x6b175474e89094c44da98b954eedeac495271d0f'); // dai
+  const [to, setTo] = useState('0xeb919ADce5908185A6F6C860Ab42812e83ED355A'); // dai
   const [value, setValue] = useState('0');
   const [data, setData] = useState('0x00');
   const [txHash, setHash] = useState('');
 
-  const getAddress = () => {
+  // Sign Message
+  const [message, setMessage] = useState('');
+  const [messageSignature, setMessageSignature] = useState('');
+
+  // Loading status
+  const [isGettingAddress, setIsGettingAddress] = useState(false);
+  const [isSigningTransaction, setIsSigningTx] = useState(false);
+  const [isSigningMessage, setIsSigningMsg] = useState(false);
+
+  const getAddress = async () => {
+    setIsGettingAddress(true);
     const addressIdx = parseInt(addressIndex);
-    ETH.getAddress(addressIdx).then((address) => {
+    try {
+      const address = await ETH.getAddress(addressIdx); //.then((address) => {
       setAddress(address);
       web3.eth.getBalance(address, 'pending', (err, balance) => {
         setBalance(web3.utils.fromWei(balance));
       });
-    });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsGettingAddress(false);
+    }
   };
 
   const gasHandler = (gasPrice) => {
@@ -44,49 +60,68 @@ function EthTest({ transport, appPrivateKey, appId }) {
     setGasPrice(gasPriceInGWei);
   };
 
-  const signTx = () => {
+  const signTx = async () => {
+    setIsSigningTx(true);
     // transfer dai
     // const data = '0xa9059cbb000000000000000000000000c94f3bebddfc0fd7eac7badb149fad2171b94b6900000000000000000000000000000000000000000000000000000000000003e8'
-    web3.eth.getTransactionCount(address, 'pending').then((nonce) => {
-      // Get latest nonce
-      web3.eth.estimateGas({ to, data }, (err, gasLimit) => {
-        // Get gasLimit
+
+    try {
+      const nonce = await web3.eth.getTransactionCount(address, 'pending'); // .then((nonce) => {
+
+      let gasLimit;
+      try {
+        gasLimit = await web3.eth.estimateGas({ to, data }); //, (err, gasLimit) => {
+      } catch (error) {
+        gasLimit = 21000;
+      }
+
+      const gasLimitHex = web3.utils.toHex(gasLimit);
+      const gasPriceHex = web3.utils.toHex(web3.utils.toWei(gasPrice.toString(), 'Gwei'));
+      const param = {
+        chainId,
+        nonce: web3.utils.toHex(nonce),
+        gasPrice: gasPriceHex,
+        gasLimit: gasLimitHex,
+        to,
+        value: web3.utils.toHex(web3.utils.toWei(value.toString(), 'ether')),
+        data,
+        // tokenInfo: {
+        //   symbol: 'DAI',
+        //   decimals: 18,
+        // },
+      };
+
+      const signedTx = await ETH.signTransaction(param, addressIndex); //.then((signedTx) => {
+
+      web3.eth.sendSignedTransaction(signedTx, (err, txHash) => {
         if (err) {
-          console.error(`estimate gas failed`, err);
-        }
-        if (!gasLimit) gasLimit = 21000;
-        const gasLimitHex = web3.utils.toHex(gasLimit);
-        const gasPriceHex = web3.utils.toHex(web3.utils.toWei(gasPrice.toString(), 'Gwei'));
-        const param = {
-          chainId,
-          nonce: web3.utils.toHex(nonce),
-          gasPrice: gasPriceHex,
-          gasLimit: gasLimitHex,
-          to,
-          value: web3.utils.toHex(web3.utils.toWei(value.toString(), 'ether')),
-          data,
-          tokenInfo: {
-            symbol: 'DAI',
-            decimals: 18,
-          },
-        };
-        console.log(param);
-        ETH.signTransaction(param, addressIndex).then((signedTx) => {
-          console.log(signedTx);
-          web3.eth.sendSignedTransaction(signedTx, (err, txHash) => {
-            if (err) {
-              console.error(err);
-            } else setHash(txHash);
-          });
-        });
+          console.error(err);
+        } else setHash(txHash);
       });
-    });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsSigningTx(false);
+    }
+  };
+
+  const signMessage = async (message) => {
+    setIsSigningMsg(true);
+    try {
+      const addrIndex = 0;
+      const signature = await ETH.signMessage(message, addrIndex);
+      console.log(`Full Message Signature: ${signature}`)
+      setMessageSignature(signature);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsSigningMsg(false);
+    }
   };
 
   return (
     <Container style={{ textAlign: 'left' }}>
       <h5> Get Address</h5>
-      {/* Get Address from Card */}
       <Row>
         <Col xs={3}>
           <InputGroup className='mb-3'>
@@ -99,8 +134,13 @@ function EthTest({ transport, appPrivateKey, appId }) {
               aria-describedby='basic-addon2'
             />
             <InputGroup.Append>
-              <Button variant='outline-success' compact='true' onClick={getAddress}>
-                Get Address
+              <Button
+                disabled={isGettingAddress}
+                variant='outline-success'
+                compact='true'
+                onClick={getAddress}
+              >
+                {isGettingAddress ? 'Loading...' : 'Get Address'}
               </Button>
             </InputGroup.Append>
           </InputGroup>
@@ -159,8 +199,8 @@ function EthTest({ transport, appPrivateKey, appId }) {
               </Form.Group>
               <Form.Group as={Col} xs={2}>
                 <Form.Label> Send </Form.Label>
-                <Button variant='outline-success' onClick={signTx}>
-                  Sign & Send
+                <Button disabled={isSigningTransaction} variant='outline-success' onClick={signTx}>
+                  {isSigningTransaction ? 'Signing Tx...' : 'Sign & Send'}
                 </Button>
               </Form.Group>
             </Form.Row>
@@ -172,23 +212,30 @@ function EthTest({ transport, appPrivateKey, appId }) {
           <p style={{ textAlign: 'left', fontSize: 20 }}> {txHash} </p>
         </Col>
       </Row>
+      <h5>Sign Message</h5>
       <Row>
+        <Col xs={4}>
+          <FormControl
+            value={message}
+            placeholder='Message to sign'
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          ></FormControl>
+        </Col>
         <Col>
           <Button
+            disabled={isSigningMessage}
             variant='outline-success'
-            onClick={() => {
-              const addrIndex = 0;
-              const message = '436f6f6c57616c6c65744973436f6f6c';
-              const publicKey =
-                '033a057e1f19ea73423bd75f4d391dd28145636081bf0c2674f89fd6d04738f293';
-              ETH.signMessage(message, addrIndex, publicKey).then((signature) => {
-                console.log(`signature`, signature);
-              });
-            }}
+            onClick={() => signMessage(message)}
           >
-            {' '}
-            SignMessage{' '}
+            {isSigningMessage ? 'Signing...' : 'Sign Message'}
           </Button>
+        </Col>
+        <Col>
+          <p style={{ textAlign: 'left', fontSize: 20 }}> {
+            messageSignature ? messageSignature.slice(0, 20) + '...' : ''
+          } </p>
         </Col>
       </Row>
     </Container>
