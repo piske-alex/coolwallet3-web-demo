177336671247d9dd42174b39f32971c54bff28bd
ina.tsai1 <ian.tsai@coolbitx.com>
2020-06-20 16:22:22 +0800

    add switches

--- src/Components/WalletManagement/index.js
+++ src/Components/WalletManagement/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Wallet from './Wallet'
 import Settings from './Settings'
+import Switches from './Switches'
 import CoolWallet from '@coolwallets/wallet';
 
 function WalletManagement ({ appPrivateKey, appPublicKey, appId, transport }) {
@@ -11,6 +12,7 @@ function WalletManagement ({ appPrivateKey, appPublicKey, appId, transport }) {
     <>
       <Wallet wallet={wallet}/>
       <Settings wallet={wallet} appPublicKey={appPublicKey} />
+      <Switches />
     </>
   )
 }
