ebd71fdcebd2c5481a53caf6084ca509b58c06be
Quincy Chang <quincy0710@hotmail.com>
2020-08-31 14:47:36 +0800

    add quarkchain

--- src/Components/QuarkChain/index.js
+++ src/Components/QuarkChain/index.js
@@ -85,6 +85,11 @@ async function getAccountData(address) {
  * @param {string} data
 */
 async function estimateGas(from, to, value, data) {
+	console.log('from :', from);
+	console.log('to :', to);
+	console.log('value :', value);
+	console.log('data :', data);
+	console.log('estimateGas start');
 	if (!value) value = '0x';
 	if (!data) data = '0x';
 	const estimatedGas = await request('estimateGas', [{from, to, value, data}]);
@@ -96,23 +101,21 @@ async function estimateGas(from, to, value, data) {
  * @param {string} shardKey
 */
 async function getGasPrice(shardKey) {
-	const gasPrice = await request('gasPrice', [shardKey, '0x8bb0']);
-	console.log('gasPrice :', BigNumber(gasPrice, 16).shiftedBy(-18).toFixed(), 'QKC');
-	return gasPrice;
+	return await request('gasPrice', [shardKey, '0x8bb0']);
 }
 
 function QKCTest({ transport, appPrivateKey, appId }) {
   const QKC = new cwsQKC();
 
   const [addressIndex, setAddressIndex] = useState(0);
-  const [gasPrice, setGasPrice] = useState(10);
+  const [gasPrice, setGasPrice] = useState(0);
   const [balance, setBalance] = useState(0);
 
   // sign transaction
   const [address, setAddress] = useState("");
-  const [to, setTo] = useState("0x058B0ce40163ce7a4914111D09F807b12cCA9D8F");
-  const [value, setValue] = useState("0");
-  const [data, setData] = useState("0x00");
+  const [to, setTo] = useState("0x6f25509095c18cfa232027e8d9f06bf4444b878100000000");
+  const [amount, setAmount] = useState("0");
+  const [data, setData] = useState("0x");
   const [txHash, setHash] = useState("");
 
   // Sign Message
@@ -128,8 +131,11 @@ function QKCTest({ transport, appPrivateKey, appId }) {
     setIsGettingAddress(true);
     const addressIdx = parseInt(addressIndex);
     try {
-      console.log(transport)
-      const address = await QKC.getAddress(transport, appPrivateKey, appId, addressIdx); //.then((address) => {
+			console.log('transport :', transport);
+			console.log('appPrivateKey :', appPrivateKey, typeof appPrivateKey);
+			console.log('appId :', appId, typeof appId);
+      let address = await QKC.getAddress(transport, appPrivateKey, appId, addressIdx);
+			address = address + '00000000';
       setAddress(address);
       const balance = await getBalance(address);
       setBalance(balance);
@@ -141,54 +147,37 @@ function QKCTest({ transport, appPrivateKey, appId }) {
     }
   };
 
-  const gasHandler = (gasPrice) => {
-    const gasPriceInGWei = gasPrice / 10;
-    setGasPrice(gasPriceInGWei);
-  };
-
   const signTx = async () => {
     setIsSigningTx(true);
     // transfer dai
     // const data = '0xa9059cbb000000000000000000000000c94f3bebddfc0fd7eac7badb149fad2171b94b6900000000000000000000000000000000000000000000000000000000000003e8'
 
     try {
+			const fromFullShardKey 	= `0x${address.slice(-8)}`;
       const nonce = await getTransactionCount(address, "pending");
-      const gasLimit = await estimateGas({ to, data });
-      const gasPrice = await getGasPrice();
+			const value = '0x' + BigNumber(amount, 10).shiftedBy(18).toString(16);
+      const gasLimit = await estimateGas(address, to, value, data);
+      const gasPrice = await getGasPrice(fromFullShardKey);
+			setGasPrice(gasPrice);
 
       const param = {
         chainId: '',
         nonce,
         gasPrice,
         gasLimit,
-        to,
-        value: '0x' + BigNumber('1', 10).shiftedBy(18).toString(16),
+        to: to.slice(0, -8),
+        value,
         data,
-        // tokenInfo: {
-        //   symbol: 'DAI',
-        //   decimals: 18,
-        // },
+				fromFullShardKey,
+				toFullShardKey: `0x${to.slice(-8)}`,
       };
       const signedTx = await QKC.signTransaction(transport, appPrivateKey, appId, param, addressIndex);
-      console.log("Signature: " + signedTx)
+      console.log("signedTx: " + signedTx)
 
     } catch (error) {
       console.error(error);
     } finally {
       setIsSigningTx(false);
-    }
-  };
-
-  const signMessage = async (message) => {
-    setIsSigningMsg(true);
-    try {
-      const signature = await QKC.signMessage(message, addressIndex);
-      console.log(`Full Message Signature: ${signature}`);
-      setMessageSignature(signature);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsSigningMsg(false);
     }
   };
 
@@ -248,16 +237,16 @@ function QKCTest({ transport, appPrivateKey, appId }) {
                 <Form.Label style={{ fontSize: 20 }}>Amount</Form.Label>
                 <Form.Control
                   type="value"
-                  value={value}
+                  value={amount}
                   onChange={(event) => {
-                    setValue(event.target.value);
+                    setAmount(event.target.value);
                   }}
                   placeholder="Amount in Eth"
                 />
               </Form.Group>
               <Form.Group xs={4} as={Col}>
                 <Form.Label style={{ fontSize: 20 }}> Gas (Gwei) </Form.Label>
-                <GasMenu handler={gasHandler}></GasMenu>
+			          <FormText style={{ textAlign: "left" }}> {`${BigNumber(gasPrice, 16).shiftedBy(-18).toFixed()} QKC`} </FormText>
               </Form.Group>
             </Form.Row>
 
@@ -289,33 +278,6 @@ function QKCTest({ transport, appPrivateKey, appId }) {
       <Row style={{ paddingTop: 20 }}>
         <Col ms={12}>
           <p style={{ textAlign: "left", fontSize: 20 }}> {txHash} </p>
-        </Col>
-      </Row>
-      <h4>Sign Message</h4>
-      <Row>
-        <Col xs={4}>
-          <FormControl
-            value={message}
-            placeholder="Message to sign"
-            onChange={(event) => {
-              setMessage(event.target.value);
-            }}
-          ></FormControl>
-        </Col>
-        <Col>
-          <Button
-            disabled={isSigningMessage}
-            variant="outline-success"
-            onClick={() => signMessage(message)}
-          >
-            {isSigningMessage ? "Signing..." : "Sign Message"}
-          </Button>
-        </Col>
-        <Col>
-          <p style={{ textAlign: "left", fontSize: 20 }}>
-            {" "}
-            {messageSignature ? messageSignature.slice(0, 20) + "..." : ""}{" "}
-          </p>
         </Col>
       </Row>
     </Container>
